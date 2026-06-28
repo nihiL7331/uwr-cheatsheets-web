90871728394b9b78c3695f2ece07ef40226f7c94
nihiL7331 <pujanekp6@gmail.com>
2026-06-29 00:06:53 +0200

    refactor: rename `uwr_cheatsheets_web` dir to `config`

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for uwr_cheatsheets_web project.
+WSGI config for notiid project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uwr_cheatsheets_web.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
